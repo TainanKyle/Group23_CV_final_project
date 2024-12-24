0089c98275e71da9a24aa30fc4cdfb65d6544ee9
YouSheng-Do <aaaa95067@gmail.com>
2024-12-24 22:49:19 +0800

    update bash

--- bash/room_tainan_1_1_img_learn.sh
+++ bash/room_tainan_1_1_img_learn.sh
@@ -3,5 +3,5 @@ stamp=$(date "+%Y-%m-%d_%H-%M-%S")
 log_dir="outputs/" # TODO
 prompt="a bedroom"
 scene_id="room_tainan_1_1"
-img_path="data/scenes/room_tainan_1_1/style.jpg"
+img_path="/home/ado/storage/CV_final_project_youshengdo/data/scenes/modern.jpg"
 python scripts/train_texture.py --config config/template_learn.yaml --stamp $stamp --log_dir $log_dir --prompt "$prompt" --scene_id "$scene_id" --img_path "$img_path"
