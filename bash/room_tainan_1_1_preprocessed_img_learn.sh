244225b9482a679994889b1e94b0db09104c3cfb
YouSheng-Do <aaaa95067@gmail.com>
2024-12-24 17:51:55 +0800

    test modern.jpg

---
+++ bash/room_tainan_1_1_preprocessed_img_learn.sh
@@ -0,0 +1,7 @@
+stamp=$(date "+%Y-%m-%d_%H-%M-%S")
+
+log_dir="outputs/" # TODO
+prompt="a bedroom"
+scene_id="room_tainan_1_1_preprocessed"
+img_path="/home/ado/storage/CV_final_project_youshengdo/data/scenes/modern.jpg"
+python scripts/train_texture.py --config config/template_learn.yaml --stamp $stamp --log_dir $log_dir --prompt "$prompt" --scene_id "$scene_id" --img_path "$img_path"
